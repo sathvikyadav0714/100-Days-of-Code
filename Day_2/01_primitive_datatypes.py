4ea354a7957d619ed7990ae58611ce3705efd2f7
sathvikyadav0714 <sathvikyadav0714@gmail.com>
2026-08-26 23:32:44 +0530

    day-2

---
+++ Day_2/01_primitive_datatypes.py
@@ -0,0 +1,26 @@
+# strings
+name="alice"
+
+# subscripting
+print("sathvik"[1])
+
+# concatenation
+print("123"+"456")
+# repetition
+print("123"*3)
+
+# integers
+a=123
+print(123+456)
+
+
+# large integers
+print(123456789)
+
+# floats
+
+print(1234.567)
+
+# boolean
+print(True)
+print(False)
---
+++ Day_2/02_TypeError_TypeConversion.py
@@ -0,0 +1,24 @@
+print(len("hello"))
+
+print(type("abc"))
+print(type(123))
+print(type(123.456))
+print(type(True))
+
+
+print(int("123")+int("456"))
+a="123"
+print(type(a))
+
+a=int(a)
+print(type(a))
+
+
+
+
+name=input("Enter your name: ")
+
+length=len(name)
+
+print("length of name is "+str(length))
+print("length of name is ",length)
