32b492e70c6d1e02632264ec53712d47261317c7
sathvikyadav0714 <sathvikyadav0714@gmail.com>
2026-08-30 00:06:21 +0530

    Day 04 random module with examples

--- Day_4/01_Random_Module.py
+++ Day_4/01_Random_Module.py
@@ -1,4 +1,34 @@
 import random
+import my_module    
 
-random_number=random.randint(1,20)
+# # Generate a random integer between 1 and 20
+# random_number=random.randint(1,20)
+# print(random_number)
+
+# print(my_module.favourite_number)
+
+# # Generate a random float between 0 and 1
+# random_number_1to10=random.random() 
+# print(random_number_1to10)
+
+# random_float_number=random.uniform(10,20)
+# print(random_float_number)
+
+
+
+# generate heads and tails based on random numbers
+random_number=random.random()
 print(random_number)
+if random_number<0.5 :
+    print("heads")
+else:
+    print("tails")
+
+# or
+
+random_heads_or_tails=random.randint(0,1)
+print(random_heads_or_tails)
+if random_heads_or_tails==0:
+    print("heads")
+else:
+    print("Tails")
